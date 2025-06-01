bdd815b327f8f74185701ef049a1add0caf968c8
vivek thakur <vivekthakur99000@gmail.com>
2025-06-01 17:45:51 +0530

    configured the store and applied react redux methodology

--- mega-blog-appwrite/src/App.jsx
+++ mega-blog-appwrite/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const dispath = useDispatch();
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispath(login({userData}));
+      }else{
+        dispath(logout());
+      }
+    }).finally(
+      setLoading(false)
+    );
+  }, []);
 
-  return (
-    <>
-     <h1>Blog app in appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+   
 }
 
-export default App
+export default App;
